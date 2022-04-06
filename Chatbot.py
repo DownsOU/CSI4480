5ba700a19701b2f3c91ae2e30314f7a0a79f1423
DownsOU <ddowns@oakland.edu>
2022-04-05 21:33:27 -0400

    new db files

--- Chatbot.py
+++ Chatbot.py
@@ -1,9 +1,12 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-chatbot = ChatBot("CSI4480")
+chatbot = ChatBot("CSI4480",
+                  storage_adapter='chatterbot.storage.SQLStorageAdapter',
+                  database_uri='sqlite:///database.sqlite3',
+                  logic_adapter=['chatterbot.logic.BestMatch'])
 
-conversation = [
+"""conversation = [
     "Hello",
     "Hi there!",
     "How are you doing?",
@@ -11,11 +14,34 @@ conversation = [
     "That is good to hear",
     "Thank you.",
     "You're welcome."
+]"""
+
+helpConversation = [
+    "I need help",
+    "I need more information, what is your username",
+    "My username is",
+    "To verify your account, please enter your email address",
+    "My email address is",
+    "Please supply your home address",
+    "address"
 ]
 
 trainer = ListTrainer(chatbot)
 
-trainer.train(conversation)
+#trainer.train(conversation)
+trainer.train(helpConversation)
+trainer.train(["help me",
+               "I need more information, what is your username"])
+trainer.train(["help",
+               "I need more information, what is your username"])
 
-response = chatbot.get_response("Good morning!")
-print(response)
+name=input("Enter Your Name: ")
+print("Welcome to the Bot Service! Let me know how can I help you?")
+while True:
+    request=input(name+':')
+    if request=='Bye' or request =='bye':
+        print('Bot: Bye')
+        break
+    else:
+        response=chatbot.get_response(request)
+        print('Bot:',response)
